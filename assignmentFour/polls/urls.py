d11a007a16fed9149c56daf863befc83628e467a
Sierra Grey-Coker <51929701+sgreyco@users.noreply.github.com>
2020-04-23 22:31:31 -0400

    Aesthetics in place

--- assignmentFour/polls/urls.py
+++ assignmentFour/polls/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
 from . import views
+from .models import Question
 
 app_name = 'polls'
+hello = 'hello'
 urlpatterns = [
     path('', views.IndexView.as_view(), name = 'index'),
     path('<int:pk>/', views.DetailView.as_view(), name = 'detail'),
     path('<int:pk>/results/', views.ResultsView.as_view(), name = 'results'),
-    path('<int:question_id>/vote/', views.vote, name='vote')
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('<int:question_id>/remove_c/', views.remove_choice, name = 'remove_c'),
+    # path('', views.remove_question, {}, name = 'remove_q')
+    path('add_a_question/', views.add_question, name = 'add_q'),
+    path('', views.add_choice, name = 'add_q')
+
 ]
--- assignmentFour/polls/views.py
+++ assignmentFour/polls/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
-
 from .models import Question, Choice
+from django.utils import timezone
+
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
@@ -11,16 +12,44 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last 5 published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.order_by('-pub_date')
+
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
 
+
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
 
+
+def remove_choice(request, question_id):
+    """
+    :param
+    :param request:
+    :param question_id: integer
+    :return:
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    question.choice_set.remove(pk=request.POST['choice'])
+    removal = ""
+    # remove choice from associated question
+    return HttpResponseRedirect(reverse('polls:edit', args=(question.id,)))
+
+
+def add_question(request):
+    question = Question()
+    question.question_text = request.POST['new_question_text']
+    question.pub_date = timezone.now()
+    question.save()
+    return HttpResponseRedirect(render('polls/index.html'))
+
+def add_choice(request):
+    choice = Choice()
+    choice.choice_text = 'choice text'
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -28,7 +57,8 @@ def vote(request, question_id):
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form
-        return render(request, 'polls/detail.html', {'question': question, 'error_message': "You didn't select a choice."})
+        return render(request, 'polls/detail.html',
+                      {'question': question, 'error_message': "You didn't select a choice."})
     else:
         selected_choice.votes += 1
         selected_choice.save()
